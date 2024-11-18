a855b122eeabc1c802dcac4ec6583b1eaf7596cb
ludovicdean <ludovicd58@gmail.com>
2024-11-18 23:02:01 +0100

    (BLG-53) correction des urls des bannières

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon"
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ludovic.dean.github.io',
-  base: '',
+  base: '/devendevenir',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
   output: "static"
 });
